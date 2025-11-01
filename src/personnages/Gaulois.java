cdab12173bda0a4ec30151db9ea5c926c3c4a662
Artichaud-froid <lenny07@free.fr>
2025-11-01 19:23:10 +0100

    Tp2 complet

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,6 +4,7 @@ public class Gaulois {
     private String nom;
     private int force;
     private int effetPotion = 0; //
+    private Village village;
 
     public Gaulois(String nom, int force) {
         this.nom = nom;
@@ -27,7 +28,22 @@ public class Gaulois {
         System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
         romain.recevoirCoup(forceCoup);
     }
-
+    
+    public void setVillage(Village village) {
+        this.village = village;
+    }
+    
+    public void sePresenter() {
+        System.out.print("Le Gaulois " + nom + " : \"Bonjour, je m'appelle " + nom + ". ");
+        if (this instanceof Chef) {
+            System.out.println("Je suis le chef du village " + village.getNom() + ".\"");
+        } else if (village != null) {
+            System.out.println("J'habite le village " + village.getNom() + ".\"");
+        } else {
+            System.out.println("Je voyage de villages en villages.\"");
+        }
+    }
+    
     @Override
     public String toString() {
         return "Gaulois " + nom + " (force " + force + ")";
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,12 +1,17 @@
 package personnages;
 
+import objets.Equipement;
+
 public class Romain {
     private String nom;
     private int force;
+    private Equipement[] equipements = new Equipement[2];
+    private int nbEquipement = 0;
 
     public Romain(String nom, int force) {
         this.nom = nom;
         this.force = force;
+        assert isInvariantVerified() : "Invariant non respecté : la force doit être positive ou nulle.";
     }
 
     public String getNom() {
@@ -17,13 +22,62 @@ public class Romain {
         System.out.println("Le Romain " + nom + " : \"" + texte + "\"");
     }
 
+    private boolean isInvariantVerified() {
+        return force >= 0;
+    }
+
     public void recevoirCoup(int forceCoup) {
+        assert forceCoup > 0 : "Précondition non respectée : le coup reçu doit être positif.";
+
+        int ancienneForce = force;
         force -= forceCoup;
+
         if (force > 0) {
             parler("Aïe");
         } else {
-            parler("J’abandonne !");
+            force = 0;
+            parler("J'abandonne !");
+        }
+
+        assert force < ancienneForce : "Postcondition non respectée : la force n'a pas diminué.";
+        assert isInvariantVerified() : "Invariant non respecté : la force est devenue négative.";
+    }
+
+    private void afficherEquipement(Equipement equipement) {
+        System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
+    }
+
+    public void sEquiper(Equipement equipement) {
+        switch (nbEquipement) {
+            case 2:
+                System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+                break;
+
+            case 1:
+                if (equipements[0] == equipement) {
+                    System.out.println("Le soldat " + nom + " possède déjà un " + equipement + " !");
+                } else {
+                    equipements[nbEquipement] = equipement;
+                    nbEquipement++;
+                    afficherEquipement(equipement);
+                }
+                break;
+
+            case 0:
+                equipements[nbEquipement] = equipement;	 
+                nbEquipement++;
+                afficherEquipement(equipement);
+                break;
         }
     }
-}
 
+    public static void main(String[] args) {
+        System.out.println("=== Test des énumérations et équipements ===");
+        Romain minus = new Romain("Minus", 6);
+
+        minus.sEquiper(Equipement.CASQUE);
+        minus.sEquiper(Equipement.CASQUE); 
+        minus.sEquiper(Equipement.BOUCLIER);
+        minus.sEquiper(Equipement.CASQUE);
+    }
+}
